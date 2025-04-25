51a5155a9da042351ff95c9186d5b8f78c5c6d4f
bogkoshek <114669037+bogkoshek@users.noreply.github.com>
2025-04-25 12:34:14 +0300

    Add files via upload

---
+++ scripts/trimmomatic_slurm_2.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#SBATCH --job-name=illumina_trimming
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=20gb
+#SBATCH --time=1:00:00
+#SBATCH --output=trimmomatic_%j.log
+#SBATCH --error=trimmomatic_%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=your_email@example.com
+#SBATCH --constraint=hpc
+#SBATCH --partition=short
+
+# Активация окружения
+source activate trimming
+
+# Выполнение Trimmomaticи
+trimmomatic PE -threads 8 -phred33 \
+illumina_R1_trimmed_paired.fq \
+illumina_R2_trimmed_paired.fq \
+illumina_R1_trimmed_paired_2.fq \
+illumina_R1_trimmed_unpaired_2.fq \
+illumina_R2_trimmed_paired_2.fq \
+illumina_R2_trimmed_unpaired_2.fq \
+ILLUMINACLIP:adapters.fa:2:30:10:8:true \
+LEADING:25 \
+TRAILING:25 \
+SLIDINGWINDOW:5:25 \
+MINLEN:50 \
+HEADCROP:10 \
+CROP:220
